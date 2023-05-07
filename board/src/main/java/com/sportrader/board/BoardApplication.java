2885ebf508aad2343ed0db758ddebd67cd93230e
abid <abid.mehboob@hubpay.pk>
2023-05-07 17:41:09 +0200

    all working files

--- board/src/main/java/com/sportrader/board/BoardApplication.java
+++ board/src/main/java/com/sportrader/board/BoardApplication.java
@@ -4,7 +4,7 @@ package com.sportrader.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class BoardApplication {
 
     public static void main(String[] args) {
--- board/src/main/java/com/sportrader/board/controller/GameController.java
+++ board/src/main/java/com/sportrader/board/controller/GameController.java
@@ -5,11 +5,14 @@ import com.sportrader.board.dto.GameRequest;
 import com.sportrader.board.dto.MatchSummary;
 import com.sportrader.board.dto.ScoreRequest;
 import com.sportrader.board.facade.impl.GameScoreboardFacadeImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Api(tags = "Game")
 @RequestMapping("/scoreboard")
 public class GameController {
 
@@ -20,18 +23,21 @@ public class GameController {
     }
 
     @PostMapping("/game")
+    @ApiOperation(value = "Start NewGame", nickname = "startNewGame", notes = "Start NewGame")
     public Mono<Game> startNewGame(@RequestBody GameRequest request) {
         Mono<Game> savedGame = Mono.just(facade.startMatch(request.getHomeTeam(), request.getAwayTeam()));
         return savedGame;
     }
 
     @PutMapping("/games/{gameId}/score")
+    @ApiOperation(value = "Update Score", nickname = "updateScore", notes = "Update Score")
     public Mono<Game> updateScore(@PathVariable("gameId") String gameId, @RequestBody ScoreRequest request) {
         Mono<Game> savedGame = Mono.just(facade.updateScore(Long.valueOf(gameId), request.getHomeTeamScore(), request.getAwayTeamScore()));
         return savedGame;
     }
 
     @DeleteMapping("/games/{gameId}")
+    @ApiOperation(value = "Finish Game", nickname = "finishGame", notes = "Finish Game")
     public Mono<Boolean> finishGame(@PathVariable("gameId") String gameId) {
         facade.finishMatch(Long.valueOf(gameId));
         Mono<Boolean> savedGame = Mono.just(Boolean.TRUE);
@@ -39,6 +45,7 @@ public class GameController {
     }
 
     @GetMapping("/games")
+    @ApiOperation(value = "Get All Games", nickname = "games", notes = "Get All Games")
     public Flux<MatchSummary> getGamesInProgressOrderedByScore() {
         return Flux.fromIterable(facade.getMatchesOrderedByTotalScore());
     }
